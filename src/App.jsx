f1c78a843986b06d917a149c12d1375861a587a1
Ernesto Melian <you@example.com>
2024-02-08 01:13:07 -0300

    removed env

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { MoviesGrid } from './components/MoviesGrid'
 
 function App() {
     const [movies, setMovies] = useState([]);
-    const apiUrl = import.meta.env.VITE_API_URL;
-    const apiKey = import.meta.env.VITE_API_KEY;
+    // const apiUrl = import.meta.env.VITE_API_URL;
+    // const apiKey = import.meta.env.VITE_API_KEY;
+    const VITE_API_URL='https://api.themoviedb.org/3/trending/movie/day'
+    const VITE_API_KEY='a4e8b99c61e88786d77483b08987cf82'
 
     useEffect(() => {
-        fetch(`${apiUrl}?api_key=${apiKey}`)
+        fetch(`${VITE_API_URL}?api_key=${VITE_API_KEY}`)
         .then(response => response.json())
         .then(response => setMovies(response.results))
         .catch(err => console.error(err));
