dcb90b05340e3dcc57dd11e3bc51710218446ca4
Ernesto Melian <you@example.com>
2024-02-09 16:50:45 -0300

    loading image message

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,21 @@
 import styles from "../styles/MovieCard.module.css";
+import React, { useState } from 'react';
 
 export function MovieCard({ img, name }) {
-    const moviePath = `https://image.tmdb.org/t/p/w500/${img}`
+    const moviePath = `https://image.tmdb.org/t/p/w500/${img}`;
+    const [isLoading, setIsLoading] = useState(true);
+    const handleImageLoad = () => {
+        setIsLoading(false);
+    };
     return (
-        <li className={styles.movieCard}>
-            <img className={styles.movieImage} src={moviePath} alt={`Poster of the movie ${name}`} width="230" height={345} />
+        <li className={[styles.movieCard,isLoading ? styles.loading : ''].join(" ").trim()}>
+            <img 
+                className={styles.movieImage} 
+                src={moviePath} 
+                alt={`Poster of the movie ${name}`} 
+                width="230" 
+                height={345}
+                onLoad={handleImageLoad} />
             <h3 className={styles.movieTitle}>{name}</h3>
         </li>
     )
